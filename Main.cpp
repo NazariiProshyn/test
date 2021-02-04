68a7b48cdc9b6792519253270280a5e273987bc2
NazariyProshyn <56487679+NazariyProshyn@users.noreply.github.com>
2021-02-04 21:36:08 +0200

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -10,12 +10,12 @@
 using namespace std;
 
 
-int** create_arr(int size);
-void swap(int** arr, int size);
-void vuvid(int*** arr, int size);
-int** mnozhennya(int*** arr1, int*** arr2, int size);
-int* create_raw(int**** arr2, int size, int j);
-int result(int** raw1, int** raw2, int size);
+int** create_arr(int size);///
+void swap(int* arr, int size);///
+void vuvid(int** arr, int size);///
+int** mnozhennya(int** arr1, int** arr2, int size);
+int* create_raw(int** arr2, int size, int j);///
+int result(int* raw1, int* raw2, int size);
 int main()
 {
 	//1
@@ -27,14 +27,14 @@ int main()
 	cin >> size;
 
 	int** arr = create_arr(size);
-	vuvid(&(arr), size);
+	vuvid(arr, size);
 
 	for (int i = 0; i < size; i++)
 	{
-		swap(&(arr[i]), size);
+		swap(arr[i], size);
 	}
 	cout << "RES" << endl;
-	vuvid(&(arr), size);
+	vuvid(arr, size);
 
 	//2
 
@@ -43,16 +43,16 @@ int main()
 
 	int** arr1 = create_arr(size);
 	cout << "MATR1" << endl;
-	vuvid(&(arr1), size);
+	vuvid(arr1, size);
 
 
 	int** arr2 = create_arr(size);
 	cout << "MATR2" << endl;
-	vuvid(&(arr2), size);
+	vuvid(arr2, size);
 
-	int** arr3 = mnozhennya(&(arr1), &(arr2), size);
+	int** arr3 = mnozhennya(arr1, arr2, size);
 	cout << "RES" << endl;
-	vuvid(&(arr3), size);
+	vuvid(arr3, size);
 }
 
 int** create_arr(int size)
@@ -74,61 +74,61 @@ int** create_arr(int size)
 	return arr;
 }
 
-void swap(int** arr, int size)
+void swap(int* arr, int size)
 {
-	int max = arr[0][0];
+	int max = arr[0];
 	int it = 0;
 
 	for (int i = 0; i < size; i++) {
-		if (max < arr[0][i]) {
-			max = arr[0][i];
+		if (max < arr[i]) {
+			max = arr[i];
 			it = i;
 		}
 	}
 
-	arr[0][it] = arr[0][0];
-	arr[0][0] = max;
+	arr[it] = arr[0];
+	arr[0] = max;
 }
 
-void vuvid(int*** arr, int size)
+void vuvid(int** arr, int size)
 {
 	for (int i = 0; i < size; i++) {
 		cout << "|";
 		for (int j = 0; j < size; j++) {
-			cout << arr[0][i][j] << " ";
+			cout << arr[i][j] << " ";
 		}
 		cout << "|" << endl;
 	}
 }
 
-int** mnozhennya(int*** arr1, int*** arr2, int size)
+int** mnozhennya(int** arr1, int** arr2, int size)
 {
 	int** arr = create_arr(size);
 	int* raw;
 	for (int i = 0; i < size; i++) {
-		raw = create_raw(&(arr2), size, i);
+		raw = create_raw(arr2, size, i);
 		for (int j = 0; j < size; j++)
 		{
-			arr[i][j] = result(&(arr1[0][j]), &(raw), size);
+			arr[i][j] = result(arr1[j], raw, size);
 		}
 	}
 	return arr;
 }
 
-int* create_raw(int**** arr2, int size, int j)
+int* create_raw(int** arr2, int size, int j)
 {
 	int* raw = new int[size];
 	for (int i = 0; i < size; i++) {
-		raw[i] = arr2[0][0][i][j];
+		raw[i] = arr2[i][j];
 	}
 	return raw;
 }
 
-int result(int** raw1, int** raw2, int size)
+int result(int* raw1, int* raw2, int size)
 {
 	int res = 0;;
 	for (int i = 0; i < size; i++) {
-		res += raw1[0][i] * raw2[0][i];
+		res += raw1[i] * raw2[i];
 	}
 	return res;
 }
